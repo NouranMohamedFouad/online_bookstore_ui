83cea7d090ac8dd6e0ffe18a6da1a3ed606cb08b
Rahma Mostafa <rahmamostafa282@gmail.com>
2025-03-08 22:46:38 +0200

    create userprofile frontend

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
+
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+  selectedSection: string = 'profile';
 
+  switchSection(section: string) {
+    this.selectedSection = section;
+  }
 }
