fc3addcf1482ca631eda5f5deab5c632ee9285c2
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2025-03-09 22:52:03 +0200

    upload new header styling for logged in users with routes for other components if logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { from } from 'rxjs';
 import { HeaderComponent } from './layout/header/header.component';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { AddBookComponent } from "./add-book/add-book.component";
-  
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HttpClientModule],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'book-store-mean';
