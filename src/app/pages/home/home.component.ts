474910f29d9646997d9834aaa43e160abc845412
Ahmed-Sayed-1 <ahmed2853519521@gmail.com>
2025-03-11 22:39:36 +0200

    Added slider

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,32 +1,38 @@
 import { BooksRequestsService } from '../../services/requests/books/books-requests.service';
-import {HttpRequestsService} from '../../services/requests/http-requests.service';
+import { HttpRequestsService } from '../../services/requests/http-requests.service';
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../interfaces/book';
 import { CommonModule } from '@angular/common';
 import { CeilPipe } from '../../pipe/ceil.pipe';
 import { Router } from '@angular/router';
 import { User } from '../../interfaces/user';
-import { ImageSliderComponent } from "../image-slider/image-slider.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CeilPipe, ImageSliderComponent],
+  imports: [CommonModule, CeilPipe],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  carouselImages = [
+    'assets/images/th (1).jpeg',
+    'assets/images/th (2).jpeg',
+    'assets/images/th (3).jpeg',
+    'assets/images/th (4).jpeg'
+  ];
   books: Book[] = [];
   currentPage: number = 1;
-  user !: User ;
+  userRole!: string;
   pageSize: number = 12;
   totalPages: number = 0;
 
-  constructor(private router: Router,private booksRequestsService: BooksRequestsService,private httpRequestsService :HttpRequestsService) { }
+  constructor(private router: Router, private booksRequestsService: BooksRequestsService, private httpRequestsService: HttpRequestsService) { }
 
   ngOnInit() {
     this.loadBooks();
+    this.userRole = this.httpRequestsService.getUserData().role;
+
   }
 
   loadBooks() {
@@ -34,7 +40,6 @@ export class HomeComponent implements OnInit {
       this.books = response.books;
       this.totalPages = response.totalPages;
       console.log(this.books);
-      
     });
   }
 
@@ -42,30 +47,33 @@ export class HomeComponent implements OnInit {
     this.currentPage = page;
     this.loadBooks();
   }
+
   deleteBook(id: number) {
     this.booksRequestsService.deleteBook(id).subscribe(response => {
       console.log('Book deleted successfully', response);
       this.loadBooks();
-    }
-    );
+    });
   }
 
   addBook() {
     this.router.navigate(['/add-book']);
   }
+
   updateBook(id: number) {
-    this.router.navigate(['/update-book',id]);
+    this.router.navigate(['/update-book', id]);
   }
+
   deleteAllBooks() {
     this.booksRequestsService.deleteAllBooks().subscribe(response => {
       console.log('All books deleted successfully', response);
       this.loadBooks();
     });
   }
-  
+
   viewBookDetails(id: number) {
-    this.router.navigate(['/book-details',id])
-    }
+    this.router.navigate(['/book-details', id]);
+  }
+
   onSearch(data: string) {
     this.booksRequestsService.getBooksList(this.currentPage, this.pageSize, data).subscribe((response: any) => {
       this.books = response.books;
