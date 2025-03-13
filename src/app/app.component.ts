6ebe4b2253ca8e29a33e63ffd566dfb4fdf2ec98
Aya Osama <ayoosh7891@gmail.com>
2025-03-13 21:55:56 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './layout/header/header.component';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { FooterComponent } from "./layout/footer/footer.component";
   
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HttpClientModule],
+  imports: [RouterOutlet, HeaderComponent, HttpClientModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
